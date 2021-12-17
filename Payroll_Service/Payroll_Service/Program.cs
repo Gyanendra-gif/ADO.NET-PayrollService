7123873b7282f79aa79eb607ad8c379db220c8eb
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-17 16:14:06 +0530

    [Gyanendra] Add: A Program on Backend to Add Data of Employee on Database

--- Payroll_Service/Payroll_Service/Program.cs
+++ Payroll_Service/Payroll_Service/Program.cs
@@ -6,8 +6,37 @@ namespace Payroll_Service
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book ");
-            EmployeeConfig employeeConfig = new EmployeeConfig();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Addition, 2-Delete, 3-Update, 4-View, 5-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        EmployeeData data = new EmployeeData();
+                        data.Name = "Gyanendra";
+                        data.Basic_pay = 89000.45;
+                        data.StartDate = DateTime.Now;
+                        data.gender = 'M';
+                        data.phone = "7440922090";
+                        data.Address = "Rewa";
+                        data.Department = "HR";
+                        data.Deduction = 567.34;
+                        data.Taxable_pay = 34.23;
+                        data.IncomeTax_pay = 10.23;
+                        data.Net_Pay = 1000;
+                        EmployeeConfig employeeConfig = new EmployeeConfig();
+                       var result=  employeeConfig.AddEmployee(data);
+                        if (result != null)
+                            Console.WriteLine("Data Got added Successfully");
+                        else
+                            Console.WriteLine("Provide Correct Data According to Coloumn");
+                        break;
+                    case 2: flag = false;
+                        break;
+                }
+            }
         }
     }
 }
